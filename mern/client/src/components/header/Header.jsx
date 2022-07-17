88d251a3895f7ab2b4286b7752371989092f5dd8
Jason Smith <91384381+Jason-Smith-Code@users.noreply.github.com>
2022-07-17 11:14:39 +0100

    header
    created header component and scss file

---
+++ mern/client/src/components/header/Header.jsx
@@ -0,0 +1,16 @@
+import './Header.scss';
+
+const Header = () => {
+    return (
+        <header>
+            {/* Logo */}
+            <div>Logo</div>
+            {/* Login */}
+            <div>Login</div>
+            {/* Menu */}
+            <div>Menu</div>
+        </header>
+    )
+}
+
+export default Header;
