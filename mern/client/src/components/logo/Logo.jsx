028a2b85cd341eccc61b1cb4fd5ff06bbe574215
jason-smith-code <jasonsmithcode@gmail.com>
2022-07-18 15:14:13 +0100

    logo
    separated logo component and styling

---
+++ mern/client/src/components/logo/Logo.jsx
@@ -0,0 +1,11 @@
+import './Logo.scss';
+
+const Logo = () => {
+    return (
+        <div id='logo-container'>
+            <p><span id="quiz">Quiz</span><span id="builder">Builder</span></p>
+        </div>
+    )
+}
+
+export default Logo;
