bd4280a516054ed5ac49f4d7073c322df9d066b4
jason-smith-code <jasonsmithcode@gmail.com>
2022-07-18 15:10:26 +0100

    Update index.js
    moved react router container to app

--- mern/client/src/index.js
+++ mern/client/src/index.js
@@ -1,13 +1,11 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import App from "./components/app/App";
+import ReactDOM from "react-dom/client";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
+const root = ReactDOM.createRoot(
   document.getElementById("root")
+);
+
+root.render(
+  <App />
 );
