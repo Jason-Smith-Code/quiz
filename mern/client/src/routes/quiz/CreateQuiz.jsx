84d65d739b4d554b1e758803d75acb2b6d55854d
jason-smith-code <jasonsmithcode@gmail.com>
2022-07-18 15:08:43 +0100

    Update CreateQuiz.jsx
    created a form for creating a quiz

--- mern/client/src/routes/quiz/CreateQuiz.jsx
+++ mern/client/src/routes/quiz/CreateQuiz.jsx
@@ -1,7 +1,22 @@
 import './CreateQuiz';
+import { Link } from "react-router-dom";
 
+
+
+//  Check if user is logged in
 const CreateQuiz = () => {
-  return <div></div>;
+  return (
+    <div className="form-container">
+      <form className='form-layout'>
+      <div className="form-group">
+      <label>How many questions?</label>
+        <input type="number"></input>
+      </div>
+      <input type="submit" value="Start"></input>
+      </form>
+      <Link to="/"><button className="link-button middle">Return to main page</button></Link>
+    </div>
+  )
 };
 
 export default CreateQuiz;
