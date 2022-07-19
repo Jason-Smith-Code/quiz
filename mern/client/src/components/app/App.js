85b5009b93573395983dabbcfaed8c45056cb8d8
Jason Smith <91384381+Jason-Smith-Code@users.noreply.github.com>
2022-07-19 09:10:04 +0100

    Update App.js
    revised App js

--- mern/client/src/components/app/App.js
+++ mern/client/src/components/app/App.js
@@ -1,27 +1,30 @@
 import Header from "../header/Header";
-import './App.scss';
+import "./App.scss";
 import Home from "../../routes/home/Home";
-import CreateQuiz from '../../routes/quiz/CreateQuiz';
-import CreateQuestion from '../../routes/create/CreateQuestion';
-import Login from '../login/Login';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
+import CreateQuiz from "../../routes/quiz/CreateQuiz";
+import CreateQuestion from "../../routes/create/CreateQuestion";
+import Login from "../login/Login";
+import Edit from "../../routes/edit/EditQuestion";
+import Read from "../../routes/read/Read";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
     <>
       <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route index element={<Home />} />
-        <Route path="home" element={<Home />} />
-        <Route path="create" element={<CreateQuestion />} />
-        <Route path="quiz" element={<CreateQuiz />} />
-        <Route path="login" element={<Login />} />
-      </Routes>
+        <Header />
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="home" element={<Home />} />
+          <Route path="create" element={<CreateQuestion />} />
+          <Route path="edit" element={<Edit />} />
+          <Route path="read" element={<Read />} />
+          <Route path="quiz" element={<CreateQuiz />} />
+          <Route path="login" element={<Login />} />
+        </Routes>
       </BrowserRouter>
     </>
-  );  
+  );
 };
 
 export default App;
