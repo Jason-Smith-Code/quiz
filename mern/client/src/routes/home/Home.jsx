264c21b73de9b2724414b4af94ceda1e3f26bc99
jason-smith-code <jasonsmithcode@gmail.com>
2022-07-27 12:04:22 +0100

    Update Home.jsx

--- mern/client/src/routes/home/Home.jsx
+++ mern/client/src/routes/home/Home.jsx
@@ -6,6 +6,7 @@ const Home = () => {
     return(
       <div className="content-container">
         <Link to="/create"><button className="link-button">Create questions</button></Link>
+        <Link to="/read"><button className="link-button">Read questions</button></Link>
         <Link to="/edit"><button className="link-button">View / edit Questions</button></Link>
         <Link to="/quiz"><button className="link-button">Start Quiz</button></Link>
       </div>
